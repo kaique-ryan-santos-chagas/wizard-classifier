3f5a4ba09a63c7fd73f497f3b76d515536ec980c
Kaique Ryan Santos Chagas <kaique.chagas@globalhitss.com.br>
2024-01-29 11:35:12 -0300

    Arquivo principal para o treinamento do modelo da rede neural artificial.

--- app.py
+++ app.py
@@ -25,36 +25,5 @@ trained_model = training_model.train()
 with open('model.json', 'w') as file:
     file.write(json.dumps(trained_model))
 
-
-input = [1, 1, 0, 1, 0, 0, 0, 1, 1]
-
-CORRECT_OUTPUT = 1
-
-for weights in trained_model:
-  
-  sum = 0
-  list_index = trained_model.index(weights)
-  
-  for weight in weights:
-    index = weights.index(weight)
-    sum = sum + input[index] * weight
-
-  
-  error = CORRECT_OUTPUT - sum
-
-  if round(error, 1) == 0:
-    if character_data[list_index]['Classe'] == 1:
-      talk = 'Acho que você viu a bruxa ' + character_data[list_index]['Nome']
-      tts = gTTS(talk, lang='pt')
-      tts.save('ai_talk.mp3')
-      playsound('ai_talk.mp3')
-      os.unlink(os.getcwd() + '/ai_talk.mp3')
-        
-    elif character_data[list_index]['Classe'] == 0:
-      talk = 'Acho que você viu o bruxo ' + character_data[list_index]['Nome']
-      tts = gTTS(talk, lang='pt')
-      tts.save('ai_talk.mp3')
-      playsound('ai_talk.mp3')
-      os.unlink(os.getcwd() + '/ai_talk.mp3')
         
 
